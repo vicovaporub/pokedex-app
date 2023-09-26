d2170b129bd2bd7c31a112788152900389511ebf
Victor Castro <victor.castro34@gmail.com>
2023-09-26 12:57:29 -0300

    moved GetPoke folder

---
+++ src/GetPoke/GetPoke.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useEffect, useState } from "react";
+import { Pokemon } from "@/shared/PokemonClass";
+import { PokeList } from "../components/PokeList/PokeList";
+
+export const GetPoke = () => {
+  const [pokeData, setPokeData] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  const getList = (offset = 0, limit = 151) => {
+    return fetch(
+      `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        let dataList = data.results;
+
+        Promise.all(
+          dataList.map(
+            (
+              pokePage: { url: RequestInfo | URL } //TODO: ARRUMAR PARA UM TIPO MAIS RELEVANTE
+            ) =>
+              fetch(pokePage.url)
+                .then((res) => res.json())
+                .then((res) => new Pokemon(res))
+          )
+        ).then((pokemonList: Pokemon[]) => {
+          setPokeData(pokemonList);
+        });
+      });
+  };
+
+  return (
+    <>
+    <PokeList pokeData={pokeData} />
+    </>
+  )
+
+};
