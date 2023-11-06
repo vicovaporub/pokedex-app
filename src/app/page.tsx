64176f54c02f9c8dbf7e57f9a80eb422fa647526
Victor Castro <victor.castro34@gmail.com>
2023-11-06 18:03:30 -0300

    feat: added css to search container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import { SearchComponent } from "@/components/SearchComponent/SearchComponent";
 export default function Home() {
   return (
    <>
+   <div className='search-container'>
    <SearchComponent/>
+   </div>
    </>
   )
 }
