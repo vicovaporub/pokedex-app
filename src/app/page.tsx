66eeca5bdf2513cabe02a0bdcad1127a5cb03f06
Victor Castro <victor.castro34@gmail.com>
2023-11-08 17:25:35 -0300

    fix: fixed eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
    <div className="home-disclaimer">
     <h1>Welcome to my simple Pokedex <br />
     Here you can search for details of a specific Pokemon <br />
-    with it's name or number. Or you can get a list of cards for each generation <br />
+    with its name or number. Or you can get a list of cards for each generation <br />
     for just the base stats values by clicking one of the generation buttons above</h1>
    </div>
    <div className="search-container">
